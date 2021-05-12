834461cfcd58da4f912f8accfd3a5600faf4b94f
kianak2002 <61980014+kianak2002@users.noreply.github.com>
2021-05-12 06:24:57 +0430

    Add files via upload
    bi-bfs impossible state handled  and A* complete

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+'''
+class for nodes
+we save the butter and robot for each node
+and also parent and th action from parent for path
+'''
+
 
 class node():
     def __init__(self, parent, robot, butter, action_from_par):
@@ -9,16 +15,27 @@ class node():
         self.action_from_par = action_from_par
 
 
+'''
+class for map and finding robot and butters and plates and obstacles position
+and check the available actions for both normal and reversed movements 
+'''
+
+
 class environment():
     def __init__(self, map_file):
         self.rows, self.cols = map(int, map_file.readline().split())
         self.table = [map_file.readline().split() for j in range(self.rows)]
         self.find_people()
         self.find_butter()
-
+    '''
+    to have the plates and butters position
+    '''
     def map(self):
         return self.people_list, self.butter_list
 
+    '''
+    to find plates(people) position
+    '''
     def find_people(self):
         self.people_list = []
         for i in range(self.rows):
@@ -26,26 +43,35 @@ class environment():
                 if 'p' in self.table[i][j]:
                     self.people_list.append((i, j))
 
+    '''
+    to find robot position
+    '''
     def find_robot(self):
         for i in range(self.rows):
             for j in range(self.cols):
                 if 'r' in self.table[i][j]:
                     return i, j
-
+    '''
+    to find the butters position
+    '''
     def find_butter(self):
         self.butter_list = []
         for i in range(self.rows):
             for j in range(self.cols):
                 if 'b' in self.table[i][j]:
                     self.butter_list.append((i, j))
-
-    def find_obstables(self):
+    '''
+    to find the obstacles position
+    '''
+    def find_obstacles(self):
         self.obstacle_list = []
         for i in range(self.rows):
             for j in range(self.cols):
                 if 'x' in self.table[i][j]:
                     self.obstacle_list.append((i, j))
-
+    '''
+    check all the available actions for robot and return them
+    '''
     def available_actions(self, robot, butter):
         actions = []
         if robot[0] - 1 >= 0 and self.table[robot[0] - 1][robot[1]] != "x":
@@ -76,7 +102,10 @@ class environment():
             else:
                 actions.append("R")
         return actions
-
+    '''
+    check the action given with all available actions
+    if allowed then it moves the robot and if needed the butter
+    '''
     def step(self, robot, butter, action):
         # action : "U", "D", "R", "L"
         # robot: tuple (x, y)
@@ -126,6 +155,9 @@ class environment():
         else:
             raise ValueError("action is wrong")
 
+    '''
+    check all the available actions for robot in reversed state and return them
+    '''
     def available_actions_reverse(self, robot, butter):
         actions = []
         if robot[0] - 1 >= 0 and self.table[robot[0] - 1][robot[1]] != "x":
@@ -146,6 +178,10 @@ class environment():
 
         return actions
 
+    '''
+    check the action given with all available actions for the reversed state
+    if allowed then it moves the robot and if needed the butter
+    '''
     def step_inverse(self, robot, butter, action):
         # action : "U", "D", "R", "L"
         # robot: tuple (x, y)
@@ -200,28 +236,25 @@ class environment():
             raise ValueError("action is wrong")
 
 
+'''
+class for bidirectional method and returning the path and actions
+'''
+
+
 class agent():
     def __init__(self, env):
         self.env = env
         self.butter = env.find_butter()
         self.robot = env.find_robot()
         self.people = env.find_people()
-
-    def bfs(self):
-        queue = []
-        root = node(None, self.robot, self.butter, None)
-        queue.append(root)
-        while True:
-            n = queue.pop(0)
-            if n.butter == self.people:
-                self.show_path(n)
-                break
-            actions = env.available_actions(n.robot, n.butter)
-            for a in actions:
-                next_robot, next_butter, _ = env.step(n.robot, n.butter, a)
-                child = node(n, next_robot, next_butter, a)
-                queue.append(child)
-
+    '''
+    The Main Method!!!
+    for the normal walking from the start node its as same as bfs
+    for the reversed walking the goal is butter in plate and robot next to them
+    so for reversed we start from all the states that robot is next to the plates(people)
+    returns the robot position for the other movements
+    returns the path and actions
+    '''
     def bidirectional_bfs(self, people_list, butter, robot):
         queue_r = []
         queue_p = []
@@ -230,7 +263,6 @@ class agent():
         root_r = node(None, robot, butter, None)
         queue_r.append(root_r)
 
-
         for i in range(len(people_list)):
             actions = env.available_actions(people_list[i], people_list[i])
             for a in actions:
@@ -238,25 +270,24 @@ class agent():
                 child = node(None, robot=next_robot, butter=people_list[i], action_from_par=None)
                 queue_p.append(child)
 
-        while True:
+        while len(queue_p) > 0 and len(queue_r) > 0:
             node_r = queue_r.pop(0)
             node_p = queue_p.pop(0)
             is_visited_r[node_r.robot + node_r.butter] = node_r
             is_visited_p[node_p.robot + node_p.butter] = node_p
+            ##the robots see each other:)) and the butters are in the same position
             if node_r.robot + node_r.butter in is_visited_p.keys():
                 action_path, path = self.show_bidirectional_path(node_r, is_visited_p[node_r.robot + node_r.butter])
                 n = is_visited_p[node_r.robot + node_r.butter]
                 while n.parent != None:
                     n = n.parent
                 return n.robot, action_path, path
-                # break
             if node_p.robot + node_p.butter in is_visited_r.keys():
                 action_path, path = self.show_bidirectional_path(is_visited_r[node_p.robot + node_p.butter], node_p)
                 n = node_p
                 while n.parent != None:
                     n = n.parent
                 return n.robot, action_path, path
-                # break
             actions = env.available_actions(node_r.robot, node_r.butter)
             for a in actions:
                 next_robot, next_butter, _ = env.step(node_r.robot, node_r.butter, a)
@@ -270,19 +301,11 @@ class agent():
                 if next_robot + next_butter not in is_visited_p.keys():
                     child = node(node_p, next_robot, next_butter, a)
                     queue_p.append(child)
-
-
-
-    def show_path(self, final_node):
-        n = final_node
-        print("****")
-        print(n.robot, n.action_from_par)
-        while n != None:
-            n = n.parent
-            print(n.robot, n.action_from_par)
-
-
-
+        return [], [], []
+    '''
+    building the path correctly and returns it
+    also returns the actions the same way
+    '''
     def show_bidirectional_path(self, final_node_r, final_node_p):
         path = []
         action_path = []
@@ -301,7 +324,9 @@ class agent():
             n = n.parent
             path.append(n.robot)
         return action_path, path
-
+    '''
+    just for inversing the actions for the reversed state
+    '''
     def inverse(self, action):
         if action == "U":
             return "D"
@@ -313,14 +338,26 @@ class agent():
             return "R"
 
 
+def beauty(table, cols):
+    for i in range(len(table)):
+        for j in range(cols):
+            print(table[i], sep='\t\t')
+
+
+'''
+prints the path and actions and cost and goal depth
+'''
 if __name__ == "__main__":
-    with open("test3.txt", "r") as file:
+    with open("test4.txt", "r") as file:
         env = environment(file)
     people_list, butter_list = env.map()
+    # beauty(env.table, env.cols)
     robot = env.find_robot()
     test_agent = agent(env)
     for i in range(len(butter_list)):
-        robot,action_path, path = test_agent.bidirectional_bfs(people_list, butter_list[i], robot)
+        robot, action_path, path = test_agent.bidirectional_bfs(people_list, butter_list[i], robot)
+        if path == []:
+            print("Impossible")
         print("path:", path)
         print("actions:", action_path)
         print("cost:", len(action_path))
